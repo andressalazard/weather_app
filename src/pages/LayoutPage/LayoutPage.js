fe8096f4e0f7eacaae0f85f558569e32abb3d33e
andressalazard <andres.salazar.d.18@gmail.com>
2021-08-18 11:05:21 -0500

    Build the main Layout of the app

---
+++ src/pages/LayoutPage/LayoutPage.js
@@ -0,0 +1,10 @@
+import styles from './LayoutPage.module.css';
+
+export default function LayoutPage(){
+  return(
+  <div className={styles.container}>
+    <div>Here goes the Summary</div>
+    <div>Here goes the Info Section</div>  
+  </div>
+  )
+}
