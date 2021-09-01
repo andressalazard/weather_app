16d658e7cbce06aafef5d4137902f00f7eb9d41d
andressalazard <andres.salazar.d.18@gmail.com>
2021-09-01 12:11:01 -0500

    handling input changes and modify the results presentation

--- src/components/Summary-components/SearchingZone/SearchingZone.js
+++ src/components/Summary-components/SearchingZone/SearchingZone.js
@@ -7,8 +7,12 @@ export default class SearchingZone extends React.Component{
     super(props);
 
     this.state = {
-      stylesList : [styles.container]
+      stylesList : [styles.container],
+      searchValue : ''
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidUpdate(prevProps){
@@ -26,8 +30,16 @@ export default class SearchingZone extends React.Component{
           this.setState({stylesList})
         }
       }
-    }
-    
+    } 
+  }
+
+  handleChange(event){
+    this.setState({searchValue: event.target.value});
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    console.log('valor ingresado: ',this.state.searchValue);
   }
   
   render(){
@@ -40,18 +52,50 @@ export default class SearchingZone extends React.Component{
                 close
               </span>
             </button>
-            <form className={styles.search_form}>
+            <form className={styles.search_form}
+              onSubmit={this.handleSubmit}>
               <div className={styles.input_field}>
                 <span className={"material-icons"}>search</span>
-                <input type='text' name='search' placeholder="search location"/>
+                <input type='text' name='search' 
+                  placeholder="search location"
+                    value={this.state.searchValue}
+                    onChange={this.handleChange}
+                  />
               </div>
-              <button type='submit'>Search</button>
+              {/* <button type='submit'>Search</button> */}
+              <input className= {styles.submit_button} type='submit' value='Search'/>
             </form>
           </div>
 
+          {this.displayResults()}
+  
       </div>
     )
   }
 
+  displayResults(){
+    let array = ['value1','value2','value3','value4','value5'];
 
+    return (
+      <div className={styles.search_results}>
+        {array.map((value, index) => {
+          return(<SearchResult value={value} key={index}/>)
+        })}
+      </div>
+    )
+
+  }
+
+
+}
+
+const SearchResult = (props) => {
+  let { value } = props;
+
+  return(
+    <div className={styles.result_container}>
+      <h1>{value}</h1>
+      <span className={["material-icons",styles.result_icon].join(' ')}>navigate_next</span>
+    </div>
+  )
 }
